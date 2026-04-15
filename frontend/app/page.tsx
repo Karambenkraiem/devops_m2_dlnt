1636b7fc7ebbb1fff2033355afe90140d5dd9d05
karamBenKraiem <karam.benkraiem@gmail.com>
2026-04-15 15:46:35 +0100

    test ci-cd from local 1

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -111,7 +111,7 @@ export default function HomePage() {
         }}
       >
         <Typography variant="h3" fontWeight="bold" gutterBottom>
-          Bienvenue DataServTech
+          Bienvenue DataServ-Tech
         </Typography>
 
         <Typography variant="h6" color="text.secondary" sx={{ mb: 4 }}>
